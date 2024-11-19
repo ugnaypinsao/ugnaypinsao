e283efe3916ebb0212a5108dace8ced27f371711
ugnaypinsao <ugnaypinsao@gmail.com>
2024-11-19 13:24:39 +0800

    Add files via upload

---
+++ res/js/loadContent.js
@@ -0,0 +1,3 @@
+document.getElementById("intro-body").innerHTML = `
+    <p>Welcome to Barangay Pinsao Proper, a place where tradition meets natural beauty. Explore the unique landscapes of <i>Stone Kingdom</i>, savor local flavors at <i>Farmer's Daughter</i> Restaurant, and enjoy the peaceful views from <i>Tam-awan Village</i>. Here, every corner has a story, and we're excited to share our vibrant community with you!</p>
+`;
